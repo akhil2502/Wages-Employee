b5d6ceb592f35419bf40ebb80b196680a06d302e
akhil2502 <akhilachilies>
2021-07-31 19:48:29 +0530

    [akhil] Add: uc7-class

--- class.cs
+++ class.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Text;
 
-
 namespace EmployeeWageComputation
 {
     class EmployeeWage
@@ -17,6 +16,7 @@ namespace EmployeeWageComputation
         int totalMonthlyWage = 0;
         int totalEmpHrs = 0;
         int totalWorkingDays = 0;
+
         public void MonthWage()
         {
             while (totalEmpHrs < MAX_HRS_IN_MONTH &&
@@ -25,18 +25,12 @@ namespace EmployeeWageComputation
                 totalWorkingDays++;
                 Random rd = new Random();
                 int empCheck = rd.Next(0, 3);
-                switch (empCheck)
+                empHrs = empCheck switch
                 {
-                    case IS_FULL_TIME:
-                        empHrs = 8;
-                        break;
-                    case IS_PART_TIME:
-                        empHrs = 4;
-                        break;
-                    default:
-                        empHrs = 0;
-                        break;
-                }
+                    IS_FULL_TIME => 8,
+                    IS_PART_TIME => 4,
+                    _ => 0,
+                };
                 totalEmpHrs += empHrs;
             }
             totalMonthlyWage = totalEmpHrs * EMP_RATE_PER_HR;
