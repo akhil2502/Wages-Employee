58802215defa780226eb4cd9eaca444b5d58d8ad
akhil2502 <akhilachilies>
2021-07-31 19:35:01 +0530

    [akhil] Add: uc4-switch

--- switch.cs
+++ switch.cs
@@ -6,26 +6,31 @@ namespace EmployeeWageComputation
 {
     class EmployeeWage
     {
-
         const int IS_PART_TIME = 1;
         const int IS_FULL_TIME = 2;
         const int EMP_RATE_PER_HR = 20;
 
-      
         int totalWage = 0;
         int empHrs = 0;
-        
+
         public void dailyWage()
         {
             Random rd = new Random();
             int empCheck = rd.Next(0, 3);
-            if (empCheck == 2)
-                empHrs = 8;
-            else if (empCheck == 1)
-                empHrs = 4;
-            else
-                empHrs = 0;
+            switch (empCheck)
+            {
+                case 2:
+                    empHrs = 8;
+                    break;
+                case 1:
+                    empHrs = 4;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
+            }
             totalWage = EMP_RATE_PER_HR * empHrs;
             Console.WriteLine("Employee daily wage:" + totalWage);
         }
     }
+}
