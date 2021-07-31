bbc640350d25e033d94614ec13cef836864fb550
akhil2502 <akhilachilies>
2021-07-31 19:01:22 +0530

    [akhil] Add: Welcome

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Wage_employee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome");
+        }
+    }
+}
