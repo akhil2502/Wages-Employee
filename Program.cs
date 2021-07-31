4cf406262079ce0c8a500a067b66f73fb45decb0
akhil2502 <akhilachilies>
2021-07-31 19:55:20 +0530

    [akhil] Add: uc8-multiple

--- Program.cs
+++ Program.cs
@@ -6,8 +6,9 @@ namespace Wage_employee
     {
         static void Main(string[] args)
         {
-            EmployeeWage empWage = new EmployeeWage();
-            empWage.monthWage();
+            EmployeeWage.ComputeEmpWage("WalMart", 30, 2, 10);
+            EmployeeWage.ComputeEmpWage("Dmart", 20, 2, 10);
+            EmployeeWage.ComputeEmpWage("Reliance", 10, 4, 20);
         }
     }
 }
---
+++ multiple.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWageComputation
+{
+    class EmployeeWage
+    {
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+
+        static int empHrs = 0;
+        static int totalEmpHrs = 0;
+        static int totalWorkingDays = 0;
+        static int totalEmpWage = 0;
+        public static int ComputeEmpWage(string company, int empRatePerHour, int numOfWokingDays, int maxHoursPerMonth)
+        {
+            while (totalEmpHrs < maxHoursPerMonth &&
+                    totalWorkingDays < numOfWokingDays)
+            {
+                totalWorkingDays++;
+                Random rd = new Random();
+                int empCheck = rd.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Day#:" + totalWorkingDays + " Emp Hr: " + empHrs);
+            }
+            totalEmpWage = totalEmpHrs * empRatePerHour;
+            Console.WriteLine("Total Employee Wage for company " + company + ":" + totalEmpWage);
+            return totalEmpWage;
+        }
+    }
+}
